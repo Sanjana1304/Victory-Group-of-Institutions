65784b5bec39c2f0d9b169de834c85bb8120077d
Sanjana Acharya <sensanjana072@gmail.com>
2024-09-21 13:02:27 +0530

    modified sidebar colors

--- frontend/src/components/landBox/SideBar.jsx
+++ frontend/src/components/landBox/SideBar.jsx
@@ -4,14 +4,14 @@ const Sidebar = ({activeItem,setActiveItem}) => {
     const options = ["Programming & Development","Office Productivity Tools","Business & Accounting Solutions","Spoken Languages","Creative Design & Multimedia", "Tuitions","Summer Camp"]
 
     return (
-        <div className="w-64  bg-pink-800 text-white flex flex-col rounded">
+        <div className="w-64  bg-gradient-to-b from-cyan-500 to-purple-400 text-white flex flex-col rounded">
                 <nav className="flex-grow">
                     <ul>
                         {
                             options.map((opt)=>(
                                 <li
                                 key={opt}
-                                className={`p-5 text-xl rounded cursor-pointer ${activeItem === opt ? 'bg-pink-700' : 'hover:bg-pink-700'}`}
+                                className={`p-5 text-xl rounded cursor-pointer ${activeItem === opt ? 'bg-gradient-to-b from-cyan-400 to-cyan-600' : 'hover:bg-gradient-to-b from-cyan-300 to-purple-600'}`}
                                 onClick={() => setActiveItem(opt)}
                             >
                                 {opt}
