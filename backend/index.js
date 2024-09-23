43dc8e0a2faac042d39b847d87b3a92c165cb8db
Sanjana Acharya <sensanjana072@gmail.com>
2024-09-23 13:42:32 +0530

    added auth and register api endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,50 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+//const bodyParser = require("body-parser");
 const express = require('express');
+const userRouter = require("./routes/userRoute");
+const authRouter = require("./routes/authRoute");
+const verifyToken = require("./middleware/authMdl");
+
 const app = express();
-const PORT = 3000;
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(
+    cors({
+        origin : process.env.FRONTEND_URL,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials : true,
+    })
+)
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log('MongoDB connected successfully hehe');
+})
+.catch(err => {
+  console.error('Ayo Error connecting to MongoDB:', err);
+});
 
 // Define a route
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello,Sanj from the backend !!');
+});
+
+app.use("/api/auth",authRouter);
+app.use("/api/users",userRouter);
+
+app.get('/protectedRoute', verifyToken, (req, res) => {
+  res.json({ message: 'This is a protected route', user: req.user });
 });
 
 // Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ backend/schema/userSchemaCode.js
@@ -0,0 +1,34 @@
+
+const mongoose = require("mongoose");
+bcrypt = require("bcryptjs");
+const userSchema = new mongoose.Schema({
+    email:{
+        unique: true,
+        type: String,
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    },
+    usertype:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+
+})
+
+userSchema.pre("save",async function(next){
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8)
+    }
+    next();
+});
+
+const userSchemaModel = mongoose.model('users',userSchema);
+
+module.exports = userSchemaModel;
