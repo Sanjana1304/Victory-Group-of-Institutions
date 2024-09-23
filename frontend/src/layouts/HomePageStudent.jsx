ff611f9cc513b2fa4982a6195b2cdb37442dc041
Sanjana Acharya <sensanjana072@gmail.com>
2024-09-23 19:38:17 +0530

    profile details displayed

--- frontend/src/layouts/HomePageStudent.jsx
+++ frontend/src/layouts/HomePageStudent.jsx
@@ -161,18 +161,18 @@ const HomePageStudent = () => {
           <p className='text-2xl font-semibold mb-3 mx-auto'>Personal Details</p>
           <img src='https://via.placeholder.com/200' alt='profile' className='mx-auto rounded-full' />
           <button className='bg-blue w-max mx-auto mt-1 rounded text-white p-1 text-[10px] cursor-pointer'>Add Photo</button>
-          <p className='text-xl font-semibold mt-2 mx-auto'>Sanjana S Acharya</p>
-          <p className='text-sm font-normal mt-2 mx-auto'>sensanjana072@gmail.com</p>
-          <p className='text-sm font-normal mx-auto'>+91 9876543210</p>
+          <p className='text-xl font-semibold mt-2 mx-auto'>{userdata.name}</p>
+          <p className='text-sm font-normal mt-2 mx-auto'>{userdata.email}</p>
+          <p className='text-sm font-normal mx-auto'>{userdata.phone}</p>
 
           <div className='mt-4 mx-auto'>
             <h1 className='text-xl font-semibold'>Education</h1>
             {
-              !isEduAdded ? 
+              !userdata.education ? 
               <button className='bg-blue w-max mx-auto mt-1 rounded text-white p-1 text-[10px] cursor-pointer'>Add Education</button>
               :
               <div>
-                <p className='text-sm '>B.Tech in CSE @ PU</p>
+                <p className='text-sm '>{userdata.education}</p>
               </div>
             }
             
@@ -181,17 +181,35 @@ const HomePageStudent = () => {
           <div className='mt-4 mx-auto flex flex-col justify-center'>
             <h1 className='text-xl font-semibold'>Work Experience</h1>
             {
-              !isWorkAdded ? 
+              !userdata.experience ? 
               <button className='bg-blue w-max mx-auto mt-1 rounded text-white p-1 text-[10px] cursor-pointer'>Add Work Experience</button>
               :
-              <p className='text-sm'>SDE @ Google</p>
+              <p className='text-sm'>{userdata.experience}</p>
             }
             
           </div>
         </div>
 
         <div className='w-[100%] sm:w-[70%] border p-4'>
-          bye
+          <h1>Courses Enrolled</h1>
+          {
+            userdata.courses.length>0
+            ?
+            <div className='mt-4'>
+              {
+                userdata.courses.map((course,index) => {
+                  return (
+                    <div key={index} className='border p-2 mb-2'>
+                      <p className='text-xl font-semibold'>{course.name}</p>
+                      <p className='text-sm font-normal'>{course.description}</p>
+                    </div>
+                  )
+                })
+              }
+            </div>
+            :
+            <p>'No Courses Enrolled'</p>
+          }
         </div>
       </main>
     </div>
