cc00ff7970fd72c52419544173b5ca726462f813
Sanjana Acharya <sensanjana072@gmail.com>
2024-10-01 14:58:11 +0530

    updated backend url in the frontend

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://victory-backend.vercel.app/',
     withCredentials: true, // Include cookies in requests
     headers: {
         'Content-Type': 'application/json'
