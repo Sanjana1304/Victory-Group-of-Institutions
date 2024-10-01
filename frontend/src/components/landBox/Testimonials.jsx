c6b51c101ffc323c0be728ddfe565c28f9fdb4ee
Sanjana Acharya <sensanjana072@gmail.com>
2024-10-01 15:28:15 +0530

    frontend deployment setup added

--- frontend/src/components/landBox/Testimonials.jsx
+++ frontend/src/components/landBox/Testimonials.jsx
@@ -26,7 +26,7 @@ const Testimonials = () => {
         <section id="testi" className="mt-10 testimonials shadow h-[300px] w-[90%] mx-auto p-5 rounded-full mb-10 bg-gradient-to-r from-cyan-100 to-purple-100">
             <h1 className="flex text-xl md:text-4xl mt-7 mb-7 text-blue justify-center font-bold">Testimonials</h1>
             <div className="testimonial-container relative overflow-hidden w-full flex flex-col justify-center items-center align-items-center">
-                {testimonialsData.map((testimonial, index) => (
+                {testimonialsData.length>0 && testimonialsData?.map((testimonial, index) => (
                     <div
                         key={index}
                         className={`testimonial ${index === currentIndex ? 'block' : 'hidden'}`}
@@ -37,7 +37,7 @@ const Testimonials = () => {
                 ))}
             </div>
             <div className="dots flex justify-center mt-4">
-                {testimonialsData.map((_, index) => (
+                {testimonialsData.length>0 && testimonialsData?.map((_, index) => (
                     <span
                         key={index}
                         className={`dot h-4 w-4 mx-1 rounded-full cursor-pointer ${index === currentIndex ? 'bg-blue' : 'bg-gray-300'}`}
